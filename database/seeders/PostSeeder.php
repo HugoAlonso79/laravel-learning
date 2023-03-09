056892b0217f299ac36258121196c151e6e1d66d
HugoAlonso79 <34077871+HugoAlonso79@users.noreply.github.com>
2023-03-09 09:51:55 -0500

    seeders

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use App\Models\Post;
+use Faker\Provider\Lorem;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        Post::truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        
+        for ($i=0; $i < 30; $i++) { 
+            $c = Category::inRandomOrder()->first();
+            $title = Str::random(20);
+            Post::create([
+                'title' => $title,
+                'slug' => Str::slug($title),
+                'content' => "<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam, distinctio nihil officia rerum ipsam sunt debitis veritatis rem consectetur, totam maiores consequuntur. Autem nesciunt excepturi cum repellat, sint exercitationem. Hic!</p>",
+                'category_id' => $c->id,
+                'description' => "<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam, distinctio nihil officia rerum ipsam sunt debitis veritatis rem consectetur, totam maiores consequuntur. Autem nesciunt excepturi cum repellat, sint exercitationem. Hic!</p>",
+                'posted' => 'yes'
+            ]);
+        }
+        
+    }
+}
--- resources/views/components/web/post/show.blade.php
+++ resources/views/components/web/post/show.blade.php
@@ -1,7 +1,6 @@
 <div>
-    <!-- He who is contented is rich. - Laozi -->
     {{-- {{ $changeTitle() }} --}}
     <h1 class="text-2xl"> {{$post->title}}</h1>
     <p> {{$post->created_at}}</p>
-    <p> {{$post->content}}</p>
+    <p>{{$post->content}}</p>
 </div>
